bfa7ace4bef0f803b0c626657ebb17c14244c970
detapundit <144154692+detapundit@users.noreply.github.com>
2023-09-19 12:12:37 +0530

    Update Basic_SQL.sql

--- Labs/Basic_SQL.sql
+++ Labs/Basic_SQL.sql
@@ -1,24 +1,24 @@
-# Some basic SQL operations
+-- Some basic SQL operations
 
-1. Create a database
+-- 1. Create a database
      create database database_name;
 
-2. Select a database
+-- 2. Select a database
      show databases;
      use database_name;
 
-3. Display list of tables inside a database
+-- 3. Display list of tables inside a database
      use database_name
      show tables;
 
-4. Check/View the structure of a table
+-- 4. Check/View the structure of a table
      show create table table_name;
      desc table_name;
 
-5. Find what is the primary key for a table
+-- 5. Find what is the primary key for a table
      show create table table_name;
    
-6. Create a table with primary key
+-- 6. Create a table with "PRIMARY key"
      | employees | CREATE TABLE `employees` (
       `emp_no` int NOT NULL,
       `birth_date` date NOT NULL,
@@ -29,7 +29,7 @@
       PRIMARY KEY (`emp_no`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
 
-7. Column with a Unique key
+-- 7. Column with a "Unique key"
      | departments | CREATE TABLE `departments` (
       `dept_no` char(4) NOT NULL,
       `dept_name` varchar(40) NOT NULL,
@@ -37,7 +37,7 @@
        UNIQUE KEY `dept_name` (`dept_name`)
        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
 
-9. Column with a Foreign key
+-- 9. Column with a "Foreign key"
       | salaries | CREATE TABLE `salaries` (
         `emp_no` int NOT NULL,
         `salary` int NOT NULL,
@@ -47,7 +47,7 @@
         CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
 
-10. Indexed column
+-- 10. Indexed column "KEY"
       | employees | CREATE TABLE `employees` (
         `emp_no` int NOT NULL,
         `birth_date` date NOT NULL,
@@ -56,6 +56,6 @@
         `gender` enum('M','F') NOT NULL,
         `hire_date` date NOT NULL,
         PRIMARY KEY (`emp_no`),
-        **KEY `idx_fname` (`first_name`)**
+        KEY `idx_fname` (`first_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
 
